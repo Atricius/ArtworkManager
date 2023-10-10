497c7bee97dd6a4af3157fb56d3172c737d47f5e
Jason Coons <137571052+Atricius@users.noreply.github.com>
2023-10-09 19:54:33 -0500

    Installed Bootstap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { GameListComponent } from './game-list/game-list.component';
 import { AddTitleComponent } from './add-title/add-title.component';
 import { ModifyTitleComponent } from './modify-title/modify-title.component';
 import { GameBrowserComponent } from './game-browser/game-browser.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { GameBrowserComponent } from './game-browser/game-browser.component';
     GameBrowserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
